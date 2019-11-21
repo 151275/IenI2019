1b8b15c1330c0d3a31d843b70cea141957a7cda8
151275 <151275@csgaugustinus.nl>
2019-11-21 09:11:01 +0000

    huisje

--- JS/H1/H1O02.js
+++ JS/H1/H1O02.js
@@ -1,12 +1,30 @@
 function setup() {
   canvas = createCanvas(450,450);
-  background('orange');
+  background('darkred');
   canvas.parent('processing');
   noLoop();
 }
 
 function draw() {
+  //pink triangle
   noStroke();
+  fill('pink');
+  triangle(0, 225, 450, 0, 450, 450);
+  //purple triangle
+  noStroke();
+  fill('purple');
+  triangle(0,0,0,450,450,225);
+  //pink red vertex
+  strokeWeight(5);
+  stroke('pink');
   fill('darkred');
-  triangle(225,0,0,450,450,450);
+  beginShape();
+  vertex(225,115);
+  vertex(300,225);
+  vertex(225,335);
+  vertex(0,225);
+  endShape(CLOSE);
+
 }
+
+
--- JS/H1/H1O04.js
+++ JS/H1/H1O04.js
@@ -9,26 +9,29 @@ function draw() {
   // teken de grond
   noStroke();
   fill('wheat');
-  rect(0,350,450,100);
+  rect(0,400,450,50);
   // teken het huis
   strokeWeight(4);
   stroke('darkgrey');
   fill('lightgray');
-  rect(100,280,100,100);
+  rect(100,330,100,100);
   fill('gray');
   // VERVANG DEZE REGEL door een regel die het dak tekent
+  triangle(100,330,)
   // teken de deur
   noStroke();
   // PAS ONDERSTAANDE REGEL AAN zodat de deur op de juiste plaats staat
-  rect(10,10,30,50);
+  rect(120,378,30,50);
   // teken de maan
   fill('khaki');
   // VERVANG DEZE REGEL door een regel die de maan tekent met een diameter van 150 en 25 vanaf de rand
+  ellipse(350,100,150)
   // teken de boom
   fill('sienna');
-  rect(300,250,40,130);
+  rect(300,300,40,130);
   fill('olive');
   // VERVANG DEZE REGEL door een regel die het groen van de boom tekent met behulp van ellipse
+  ellipse(320,260,140,200)
   // Als je een cirkel wilt tekenen met diameter 300 kun je ellipse(0,0,300) gebruiken.
   // Als je een ellipse wilt maken met breedte 300 en hoogte 500, dan moet je een parameter toevoegen: ellipse(0,0,300,500).
 }
